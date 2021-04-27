d2a48432cc8702dfc70139f38bd2b8fd4c2fabf6
iamlmn <lakshminaarayananvs@rediffmail.com>
2021-04-27 00:10:43 -0700

    completing sliding window from educative

---
+++ algos/patterns/slidingWindows/wordsConcatenation.py
@@ -0,0 +1,68 @@
+# Words Concatenation (hard) #
+# Given a string and a list of words, find all the starting indices of substrings in the 
+# given string that are a concatenation of all the given words exactly once without any 
+# overlapping of words. It is given that all words are of the same length.
+
+# Example 1:
+
+# Input: String="catfoxcat", Words=["cat", "fox"]
+# Output: [0, 3]
+# Explanation: The two substring containing both the words are "catfox" & "foxcat".
+# Example 2:
+
+# Input: String="catcatfoxfox", Words=["cat", "fox"]
+# Output: [3]
+# Explanation: The only substring containing both the words is "catfox".
+
+
+def concatenateWords(stringInput, words):
+    
+    outputIndices = []
+    wordFrequency = {}
+    wordsCount = len(words)
+    wordLength = len(words[0])
+    totalLen = wordsCount * wordLength
+
+    for i in words:
+        if i not in wordFrequency:
+            wordFrequency[i] = 0
+        wordFrequency[i] += 1
+
+
+    for i in range(len(stringInput) - totalLen + 1):
+
+        wordSeen = {}
+        for j in range(0, wordsCount):
+            word = stringInput[i + j * wordLength: i + j * wordLength + wordLength]
+
+            if word not in wordFrequency:
+                break
+
+            if word not in wordSeen:
+                wordSeen[word] = 0
+            wordSeen[word] += 1
+
+            if wordSeen[word] > wordFrequency[word]: # break if there are more occurance of a word than required.
+                break # 
+
+
+            if j + 1 == wordsCount:
+                outputIndices.append(i)
+
+
+
+    return outputIndices
+
+
+        
+
+
+
+
+if __name__ == '__main__':
+    string1 = 'catfoxcat'
+    words = ['cat', 'fox']
+    string2 = 'catcatfoxfox'
+
+    print(f" {concatenateWords(string1, words)}")
+    print(f" {concatenateWords(string2, words)}")
